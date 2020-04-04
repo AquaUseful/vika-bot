f6f7e8a5ddc281a592df4c382cb3806b18041753
RandomWP <l.vladik@outlook.com>
2020-04-05 02:48:46 +0300

    Add some getter utils

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -1,5 +1,17 @@
 import telethon
+import asyncio
+from bot import ADMINS, bot
 
-def is_private():
-    
 
+async def get_admins(chat_id, only_ids=False):
+    admins = await bot.get_participants(chat_id, filter=telethon.tl.types.ChannelParticipantsAdmins())
+    if only_ids:
+        admins = tuple(map(lambda admin: admin.id, admins))
+    return admins
+
+
+async def get_members(chat_id, only_ids=False):
+    members = await bot.get_participants(chat_id)
+    if only_ids:
+        members = tuple(map(lambda member: member.id, members))
+    return members
