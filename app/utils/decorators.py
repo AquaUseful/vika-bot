05abb3c6b597dab15d86fa8f0a64dc40f95b48ea
RandomWP <l.vladik@outlook.com>
2020-04-13 04:00:29 +0300

    Fix app decorators

--- app/utils/decorators.py
+++ app/utils/decorators.py
@@ -7,18 +7,21 @@ def req_fields(fields):
     def decorator(func):
         @functools.wraps(func)
         async def wrapper(*args, **kwargs):
-            req_json = quart.request.json
-            if all(map(lambda field: field in req_fields, fields)):
+            req_json = await quart.request.json
+            if all(map(lambda field: field in req_json, fields)):
                 return await func(*args, **kwargs)
             else:
                 await quart.abort(400, "Request missing required fields")
+        return wrapper
+    return decorator
 
 
 def token_verify(func):
     @functools.wraps(func)
     async def wrapper(*args, **kwargs):
-        req_json = quart.request.json
-        if tokens.verify_token(req_json["token"]):
+        req_json = await quart.request.json
+        if await tokens.verify_token(req_json["token"]):
             return await func(*args, **kwargs)
         else:
             await quart.abort(403, "Invalid token")
+    return wrapper
