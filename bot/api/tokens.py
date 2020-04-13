1d32582e34c1089937f1f4914990d9344e0a17a5
RandomWP <l.vladik@outlook.com>
2020-04-13 04:01:19 +0300

    Fix db util params

--- bot/api/tokens.py
+++ bot/api/tokens.py
@@ -3,10 +3,10 @@ from bot.utils import db
 
 
 async def get_chat_id_by_token(token):
-    chat = await db.get_chat_by_token({"token": token}, ["tg_id"])
+    chat = await db.get_chat_by_token(token, ["tg_id"])
     return chat["tg_id"]
 
 
 async def verify_token(token):
-    chat = await db.get_chat_by_token({"token": token})
+    chat = await db.get_chat_by_token(token)
     return bool(chat)
