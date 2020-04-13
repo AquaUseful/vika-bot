9aac3ca92aae99c9c9ffee69a38568751d81a029
RandomWP <l.vladik@outlook.com>
2020-04-13 04:00:50 +0300

    Some fixes in bot init

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,6 @@
 import asyncio
 import signal
+import sys
 from bot import config, logger, bot
 from bot.utils import utils
 
@@ -9,14 +10,15 @@ if config.CATCH_UP:
     logger.info("Catching missed updates")
     asyncio.ensure_future(utils.catch_up())
 
-
-def exit_gracefully(signal, frame):
-    asyncio.ensure_future(utils.disconnect_bot())
-    logger.info("Disconnected, stopped")
+async def exit_gracefully():
+    await utils.disconnect_bot()
+    logger.info("Disconnected, stopping...")
     asyncio.get_event_loop().stop()
 
+def signal_handler(signal, frame):
+    asyncio.ensure_future(exit_gracefully())
 
-signal.signal(signal.SIGINT, exit_gracefully)
+signal.signal(signal.SIGINT, signal_handler)
 logger.debug("Interrupt signal set")
 logger.info("Starting main event loop...")
 asyncio.get_event_loop().run_forever()
