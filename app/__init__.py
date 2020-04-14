b969663239b4679aa3907da92f153947de4ecfd2
RandomWP <l.vladik@outlook.com>
2020-04-14 22:39:15 +0300

    Add logger to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 import quart
 import hypercorn
 import os
+import logging
+
+logging.basicConfig(format="[%(asctime)s] (%(name)s) %(levelname)s: %(message)s",
+                    level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 hypercorn_cfg = hypercorn.Config()
 app = quart.Quart(__name__)
