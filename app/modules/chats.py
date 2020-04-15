4e4404eef19e24fa442bbda9156077d84b6033e0
RandomWP <l.vladik@outlook.com>
2020-04-16 02:44:42 +0300

    Add method to get chat title

--- app/modules/chats.py
+++ app/modules/chats.py
@@ -6,7 +6,7 @@ from bot.api import users as bot_users
 blueprint = quart.Blueprint("chats", __name__)
 
 
-@blueprint.route("/api/chats/get_user_ids", methods=["POST"])
+@blueprint.route("/api/chats/user_ids", methods=["POST"])
 @decorators.req_fields({"token": str})
 @decorators.token_verify
 async def user_list():
@@ -17,3 +17,10 @@ async def user_list():
     return quart.jsonify({
         "users": users
     })
+
+@blueprint.route("/api/chats/title")
+@decorators.req_fields({"token": str})
+@decorators.token_verify
+async def title():
+    req_json = await quart.request.json
+    
--- bot/api/chats.py
+++ bot/api/chats.py
@@ -5,3 +5,9 @@ async def get_chat_members(chat_id):
     chat = await db.get_chat(chat_id, ["members"])
     users = chat["members"]
     return users
+
+
+async def get_chat_title(chat_id):
+    chat = await db.get_chat(chat_id, ["title"])
+    title = chat["title"]
+    return title
