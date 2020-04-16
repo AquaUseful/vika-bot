416d2c7c69def7c6b907db421926481bece85ebd
RandomWP <l.vladik@outlook.com>
2020-04-16 13:46:54 +0300

    Add config loading

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 import quart
-import hypercorn
 import os
 
-hypercorn_cfg = hypercorn.Config()
 app = quart.Quart(__name__)
 app.config["SECRET_KEY"] = os.urandom(32)
--- app/__main__.py
+++ app/__main__.py
@@ -4,6 +4,7 @@ import signal
 import bot
 import app
 import sys
+from app import config, app
 from app.utils import blueprints
 from bot.utils import utils as bot_utils
 
@@ -15,20 +16,24 @@ def signal_handler():
     shutdown_event.set()
 
 
-@app.app.before_serving
+@app.before_serving
 async def startup():
     await bot_utils.load_modules()
     if bot.config.CATCH_UP:
         await bot_utils.catch_up()
 
 
-@app.app.after_serving
+@app.after_serving
 async def shutdown():
     await bot_utils.disconnect_bot()
 
 asyncio.ensure_future(blueprints.register_blueprints())
 
+hypercorn_cfg = hypercorn.Config()
+hypercorn_cfg.bind = [f"{config.HOST}:{config.PORT}",
+                      f"[{config.HOST_IPV6}]:{config.PORT}"]
+
 loop = asyncio.get_event_loop()
 loop.add_signal_handler(signal.SIGINT, signal_handler)
 loop.run_until_complete(hypercorn.asyncio.serve(
-    app.app, app.hypercorn_cfg, shutdown_trigger=shutdown_event.wait))
+    app, hypercorn_cfg, shutdown_trigger=shutdown_event.wait))
