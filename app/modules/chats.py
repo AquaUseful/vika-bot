2e720e4c6f8ef8abf9bd071b9f05756425fdbea1
RandomWP <l.vladik@outlook.com>
2020-04-17 02:16:17 +0300

    Some fixes

--- app/modules/chats.py
+++ app/modules/chats.py
@@ -1,5 +1,5 @@
 import quart
-from app.utils import decorators
+from app.utils import decorators, utils
 from bot.api import tokens as bot_tokens
 from bot.api import users as bot_users
 from bot.api import chats as bot_chats
@@ -7,25 +7,25 @@ from bot.api import chats as bot_chats
 blueprint = quart.Blueprint("chats", __name__)
 
 
-@blueprint.route("/api/chats/user_ids", methods=["POST"])
+@blueprint.route("/api/chats/users", methods=["POST"])
 @decorators.req_fields({"token": str})
 @decorators.token_verify
-async def user_list():
+async def users():
     req_json = await quart.request.json
     token = req_json["token"]
     chat_id = await bot_tokens.get_chat_id_by_token(token)
-    users = await bot_users.get_chat_members(chat_id)
-    return quart.jsonify({
-        "users": users
-    })
+    users = await bot_chats.get_chat_members(chat_id)
+    user_dict = {user.id: {item[0]: item[1] for item in vars(user).items() if utils.is_jsonable(item[1])}
+                 for user in users}
+    return quart.jsonify(user_dict)
 
 
-@blueprint.route("/api/chats/info")
+@blueprint.route("/api/chats/info", methods=["POST"])
 @decorators.req_fields({"token": str})
 @decorators.token_verify
 async def title():
     req_json = await quart.request.json
     token = req_json["token"]
-    chat_id = bot_tokens.get_chat_id_by_token(token)
-    chat = bot_chats.get_chat_info(chat_id)
+    chat_id = await bot_tokens.get_chat_id_by_token(token)
+    chat = await bot_chats.get_chat_info(chat_id)
     return quart.jsonify(chat)
--- bot/api/chats.py
+++ bot/api/chats.py
@@ -1,12 +1,17 @@
-from bot.utils import db
+from bot.utils import db, utils
 
 
-async def get_chat_members(chat_id):
+async def get_chat_member_ids(chat_id):
     chat = await db.get_chat(chat_id, ["members"])
     users = chat["members"]
     return users
 
 
 async def get_chat_info(chat_id):
-    chat = await db.get_chat(chat_id, {"_id": False})
+    chat = await db.get_chat(chat_id, {"_id": False, "token": False})
     return chat
+
+
+async def get_chat_members(chat_id):
+    members = await utils.get_members(chat_id)
+    return members
