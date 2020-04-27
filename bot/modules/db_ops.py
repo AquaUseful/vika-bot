27ebb0cd75f89ae4056eeaf20b623be76b085db4
RandomWP <l.vladik@outlook.com>
2020-04-27 13:03:46 +0300

    Improve chat updating

--- bot/modules/db_ops.py
+++ bot/modules/db_ops.py
@@ -1,4 +1,4 @@
-from bot import logger
+from bot import logger, bot
 from bot.utils import decorators, db, utils
 
 
@@ -6,9 +6,10 @@ from bot.utils import decorators, db, utils
 async def add_chat(event):
     chat = event.chat
     await db.add_chat_to_db(chat)
+    await bot.send_message(event.chat, "Hello! I'm Vika bot and i will help admins!"
+                           "You must give me all admin rights and use /update after that.")
 
 
-@decorators.on_chat_action
 async def update_chat(event):
     logger.debug("Got chat action, updating info...")
     members = await utils.get_members(event.chat.id, only_ids=True)
@@ -17,3 +18,14 @@ async def update_chat(event):
     await db.update_chat(event.chat.id, {"$set": {"members": members,
                                                   "admins": admins,
                                                   "banned": banned}})
+
+
+@decorators.smart_command("update")
+async def update_chat_cmd(event):
+    await update_chat(event)
+    await bot.send_message(event.chat, "Chat data updated")
+
+
+@decorators.on_chat_action
+async def update_chat_act(event):
+    await update_chat(event)
